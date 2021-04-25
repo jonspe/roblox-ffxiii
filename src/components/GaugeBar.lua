4f52c74eaab65f4b83521a8ea34734c0fbf90272
Joona Perasto <joona.perasto@tuni.fi>
2021-04-25 14:56:22 +0300

    Make GaugeBar a stateful component

--- src/components/GaugeBar.lua
+++ src/components/GaugeBar.lua
@@ -1,14 +1,17 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 local Roact = require(ReplicatedStorage.Packages.Roact)
 local RoactRodux = require(ReplicatedStorage.Packages.RoactRodux)
 
-local function GaugeBar(props)
-    local currentGauge = props.currentGauge
-    local barCount = props.barCount
+local GaugeBar = Roact.Component:extend("GaugeBar")
+
+function GaugeBar:render()
+    local currentGauge = self.props.currentGauge
+    local barCount = self.props.barCount
     local ratio = currentGauge/barCount
 
     return Roact.createElement("Frame", {
-        Size = UDim2.new(0, barCount*100, 0, 20)
+        Size = UDim2.new(0, barCount*182, 0, 20)
     }, {
         Progress = Roact.createElement("Frame", {
             Size = UDim2.new(ratio, 0, 1, 0),
